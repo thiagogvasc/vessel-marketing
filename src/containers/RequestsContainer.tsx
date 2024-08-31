1dfbbf2b87a38f03c9a691cb0df0ca9923802d7b
Thiagogvasc <thg.vasconcelos99@gmail.com>
2024-08-31 02:19:43 -0400

    Migrate to mui data grid

--- src/containers/RequestsContainer.tsx
+++ src/containers/RequestsContainer.tsx
@@ -6,101 +6,36 @@ import {
   Box,
   Typography,
   CircularProgress,
-  Grow,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
   TextField,
-  TablePagination,
   InputAdornment,
-  TableSortLabel,
-  IconButton,
-  Menu,
-  MenuItem,
-  Chip,
   Paper,
+  Chip,
 } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { useRouter } from "next/navigation";
 import { Request } from "@/src/types";
-import { MoreVert } from "@mui/icons-material";
-import theme from "@/src/theme";
-import { useTheme } from "@emotion/react";
+import { useTheme } from "@mui/material/styles";
 import React from "react";
-
+import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
+import MoreVert from "@mui/icons-material/MoreVert";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 export const RequestsContainer = () => {
   const router = useRouter();
   const { data: requests, isLoading } = useGetRequests();
-  const [showRequests, setShowRequests] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
-  const [order, setOrder] = useState<"asc" | "desc">("asc");
-  const [orderBy, setOrderBy] = useState<keyof Request>("title");
-  console.warn(requests);
-  const t = useTheme();
-  useEffect(() => {
-    if (!isLoading && requests) {
-      setShowRequests(true);
-    }
-  }, [isLoading, requests]);
-
-  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleChangePage = (event: unknown, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  const handleRequestSort = (property: keyof Request) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
-    setOrderBy(property);
-  };
-
-  const filteredRequests = requests?.filter(
-    (request) =>
-      request.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      request.description.toLowerCase().includes(searchTerm.toLowerCase()),
-  );
-
-  const sortedRequests = filteredRequests?.sort((a, b) => {
-    if (!a[orderBy] || !b[orderBy]) return 0; // Handle cases where the value is undefined
-
-    if (orderBy === "created_at" || orderBy === "updated_at") {
-      const aDate = a[orderBy];
-      const bDate = b[orderBy];
-
-      if (aDate && bDate) {
-        return aDate < bDate ? -1 : 1;
-      } else {
-        return 0;
-      }
-    }
-
-    return a[orderBy]! < b[orderBy]! ? -1 : 1;
-  });
-
-  const sortedAndPaginatedRequests = sortedRequests?.slice(
-    page * rowsPerPage,
-    page * rowsPerPage + rowsPerPage,
-  );
 
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [menuRequestId, setMenuRequestId] = useState<null | string>(null);
   const isMenuOpen = Boolean(anchorEl);
+
+  const theme = useTheme();
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
 
   const handleMenuClose = () => {
     setAnchorEl(null);
@@ -108,34 +43,123 @@ export const RequestsContainer = () => {
 
   const handleMenuOpen = (
     requestId: string,
-    event: React.MouseEvent<HTMLElement>,
+    event: React.MouseEvent<HTMLElement>
   ) => {
     setMenuRequestId(requestId);
     setAnchorEl(event.currentTarget);
   };
 
   const priorityColor = (priority: string) => {
-    if (priority === 'Low') return '#95A5A6'
-    else if (priority === 'Medium') return '#F39C12'
-    else if (priority === 'High') return '#E74C3C'
-  }
+    if (priority === "Low") return {color: '#637381', backgroundColor: 'rgba(145, 158, 171, 0.16)'};
+    else if (priority === "Medium") return { color: '#B76E00', backgroundColor: '#FFF5CC'};
+    else if (priority === "High") return { color: 'rgb(183, 29, 24)', backgroundColor: 'rgba(255, 86, 48, 0.16)'};
+  };
 
   const statusToColor = (status: string) => {
-    if (status === 'Pending') return '#F1C40F'
-    else if (status === 'In Progress') return '#3498DB'
-    else if (status === 'Completed') return '#2ECC71'
-  }
+    if (status === "Pending") return {color: '#B76E00', backgroundColor: '#FFF5CC'};
+    else if (status === "In Progress") return {backgroundColor: '#CAFDF5', color: '#006C9C'};
+    else if (status === "Completed") return { backgroundColor: 'rgba(34, 197, 94, 0.16)', color: 'rgb(17, 141, 87)'};
+  };
+
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID" },
+    { field: "title", headerName: "Title" },
+    { field: "client_id", headerName: "Customer", flex: 1 },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 130,
+      renderCell: (params: GridRenderCellParams) => (
+        <Chip
+          label={params.value}
+          sx={{
+            backgroundColor: statusToColor(params.value)?.backgroundColor,
+            color: statusToColor(params.value)?.color,
+          }}
+        />
+      ),
+    },
+    {
+      field: "priority",
+      headerName: "Priority",
+      width: 130,
+      renderCell: (params: GridRenderCellParams) => (
+        <Chip
+          label={params.value ?? "None"}
+          sx={{
+            backgroundColor: priorityColor(params.value)?.backgroundColor,
+            color: priorityColor(params.value)?.color,
+          }}
+        />
+      ),
+    },
+    { field: "assigned_to", headerName: "Assignee", width: 150 },
+    {
+      field: "created_at",
+      headerName: "Created At",
+      width: 130,
+      valueGetter: (params: any) => {
+        return new Date(params).toLocaleDateString()
+      }
+    },
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 100,
+      renderCell: (params: GridRenderCellParams<any>) => (
+        <>
+          <IconButton
+            onClick={(e) => handleMenuOpen(params.row.id as string, e)}
+          >
+            <MoreVert />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={isMenuOpen && menuRequestId === params.row.id}
+            onClose={handleMenuClose}
+          >
+            <MenuItem
+              onClick={() => {
+                handleMenuClose();
+                router.push(`/agent/requests/${params.row.id}`);
+              }}
+            >
+              View
+            </MenuItem>
+          </Menu>
+        </>
+      ),
+    },
+  ];
+
+  const filteredRequests = requests?.filter(
+    (request) =>
+      request.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      request.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const rows = filteredRequests?.map((request) => ({
+    id: request.id,
+    title: request.title,
+    client_id: request.client_id,
+    status: request.status,
+    priority: request.priority,
+    assigned_to: request.assigned_to,
+    created_at: request.created_at,
+  }));
 
   return (
     <>
-    
-        <Typography component="h1" variant="h5">
-          Requests
-        </Typography>
-        <Paper sx={{
-            boxShadow: 'rgba(145, 158, 171, 0.2) 0px 0px 2px 0px, rgba(145, 158, 171, 0.12) 0px 12px 24px -4px',
-            borderRadius: '16px'
-          }}>
+      <Typography component="h1" variant="h5">
+        Requests
+      </Typography>
+      <Paper
+        sx={{
+          boxShadow:
+            "rgba(145, 158, 171, 0.2) 0px 0px 2px 0px, rgba(145, 158, 171, 0.12) 0px 12px 24px -4px",
+          borderRadius: "16px",
+        }}
+      >
         <TextField
           variant="outlined"
           margin="normal"
@@ -171,135 +195,53 @@ export const RequestsContainer = () => {
             <CircularProgress />
           </Box>
         ) : (
-          <>
-            <TableContainer>
-              <Table>
-                <TableHead>
-                  <TableRow sx={{ border: "none" }}>
-                  <TableCell>
-                     
-                        ID
-                     
-                    </TableCell>
-                    <TableCell
-                      sortDirection={orderBy === "title" ? order : false}
-                    >
-                      <TableSortLabel
-                        active={orderBy === "title"}
-                        direction={orderBy === "title" ? order : "asc"}
-                        onClick={() => handleRequestSort("title")}
-                      >
-                        Title
-                      </TableSortLabel>
-                    </TableCell>
-                    <TableCell>
-                      Customer
-                    </TableCell>
-                    <TableCell
-                      sortDirection={orderBy === "status" ? order : false}
-                    >
-                      <TableSortLabel
-                        active={orderBy === "status"}
-                        direction={orderBy === "status" ? order : "asc"}
-                        onClick={() => handleRequestSort("status")}
-                      >
-                        Status
-                      </TableSortLabel>
-                    </TableCell>
-                    <TableCell
-                      sortDirection={orderBy === "priority" ? order : false}
-                    >
-                      <TableSortLabel
-                        active={orderBy === "priority"}
-                        direction={orderBy === "priority" ? order : "asc"}
-                        onClick={() => handleRequestSort("priority")}
-                      >
-                        Priority
-                      </TableSortLabel>
-                    </TableCell>
-                    <TableCell>
-                     
-                     Assignee
-                  
-                 </TableCell>
-                    <TableCell
-                      sortDirection={orderBy === "created_at" ? order : false}
-                    >
-                      <TableSortLabel
-                        active={orderBy === "created_at"}
-                        direction={orderBy === "created_at" ? order : "asc"}
-                        onClick={() => handleRequestSort("created_at")}
-                      >
-                        Created At
-                      </TableSortLabel>
-                    </TableCell>
-                    <TableCell>Actions</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {sortedAndPaginatedRequests?.map((request) => (
-                    <React.Fragment key={request.id ?? ''}>
-                      <TableRow>
-                      <TableCell>{request.id}</TableCell>
-                        <TableCell>{request.title}</TableCell>
-                        <TableCell>{request.client_id}</TableCell>
-                        <TableCell sx={{ color: theme.palette.text.secondary }}>
-                          <Chip label={request.status} sx={{backgroundColor: statusToColor(request.status), color: theme.palette.getContrastText(statusToColor(request.status) ?? '')}}/>
-                        </TableCell>
-                        <TableCell sx={{ color: theme.palette.text.secondary }}>
-                          {request.priority ? (
-                            <Chip label={request.priority} sx={{backgroundColor: priorityColor(request.priority), color: theme.palette.getContrastText(priorityColor(request.priority)??'')}}/>
-                          ) : (
-                            <>None</>
-                          )}
-                        </TableCell>
-                        <TableCell>
-                          {request.assigned_to}
-                        </TableCell>
-                        <TableCell sx={{ color: theme.palette.text.secondary }}>
-                          {new Date(request.created_at ?? '').toLocaleDateString()}
-                        </TableCell>
-                        <TableCell sx={{ color: theme.palette.text.secondary }}>
-                          <IconButton
-                            onClick={(e) =>
-                              handleMenuOpen(request.id as string, e)
-                            }
-                          >
-                            <MoreVert />
-                          </IconButton>
-                          <Menu
-                            anchorEl={anchorEl}
-                            open={isMenuOpen && menuRequestId === request.id}
-                            onClose={handleMenuClose}
-                          >
-                            <MenuItem
-                              onClick={() => {
-                                handleMenuClose();
-                                router.push(`/agent/requests/${request.id}`);
-                              }}
-                            >
-                              View
-                            </MenuItem>
-                          </Menu>
-                        </TableCell>
-                      </TableRow>
-                      </React.Fragment>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25]}
-              component="div"
-              count={filteredRequests?.length || 0}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
+          <Box sx={{ height: 600, width: "100%" }}>
+            <DataGrid
+              rows={rows ?? []}
+              columns={columns}
+              getRowHeight={() => 'auto'}
+              
+              // pageSize={rowsPerPage}
+              // rowsPerPageOptions={[5, 10, 25]}
+              // pagination
+              // paginationMode="client"
+              // onPageChange={(params: any) => handlePageChange(params.page)}
+              // onPageSizeChange={(params: any) => handlePageSizeChange(params.pageSize)}
+              // sortingOrder={["asc", "desc"]}
+              // sortModel={[{ field: orderBy, sort: order }]}
+              // onSortModelChange={(model) => {
+              //   if (model.length > 0) {
+              //     setOrder(model[0].sort as "asc" | "desc");
+              //     setOrderBy(model[0].field as keyof Request);
+              //   }
+              // }}
+              rowSpacingType="border"
+              sx={{
+                border: 'none', // Removes the outer border
+                '& .MuiDataGrid-row--borderBottom .MuiDataGrid-columnHeader': {
+                  border: 'none'
+                },
+                '& .MuiDataGrid-columnHeader': {
+                  backgroundColor: '#F4F6F8',
+                },
+                '& .MuiDataGrid-cell': {
+                   border: 'none',
+                  padding: '16px'
+                },
+                '& .MuiDataGrid-row': {
+                  borderBottomColor: 'rgb(145 158 171 / 20%)',
+                  borderBottomStyle: 'dashed',
+                  borderBottomWidth: '1px',
+                  borderBottomSpacing: '2px',
+                },
+                '& .MuiDataGrid-filler > *': {
+                  border: 'none'
+                }
+              }}
             />
-          </>
+          </Box>
         )}
-        </Paper>
+      </Paper>
     </>
   );
 };
